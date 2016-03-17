3d6e567079b554caf110046c9f4af4865a913d37
薛梦飞 <xuemengfei@cjia.com>
2016-03-17 11:33:04 +0800

    add scrollToBottom

--- dist/AirUI/UI/Imageclip.js
+++ dist/AirUI/UI/Imageclip.js
@@ -7,21 +7,26 @@ Air.Module("AirUI.ui.Imageclip", function(require){
 
   };
 
-  function createImageElement(imgURL){
+  function createImageElement(imgURL, width){
     var imgElement   = document.createElement('img');
     imgElement.src = imgURL;
+    imgElement.width = width;
     return imgElement;
   }
 
-  function createCameraCar(width,height){
+  function createCameraCar(width, height, x, y){
     var car = document.createElement('div');
     car.style.position = 'static';
     car.style.width = width + 'px';
     car.style.height = height + 'px';
-    car.style.top = 0;
-    car.style.left = 0;
+    car.style.top = y + 'px';
+    car.style.left = x + 'px';
     car.style.border = "1px dashed #fff";
     car.style.cursor = 'move'
+    car.style.boxSizing = 'border-box';
+    car.style.webkitBoxSizing = 'border-box';
+    car.style.mozBoxSizing = 'border-box';
+    car.style.boxSizing = 'border-box';
     return car;
   }
 
@@ -38,18 +43,23 @@ Air.Module("AirUI.ui.Imageclip", function(require){
   }
 
   function ImageClip(imgURL, options){
-    // options = merge(optons, defaultOptions);
     var imgContainer = options.imgContainer;
-    var imgElement   = createImageElement(imgURL);
-    var cameraWidth = options.width;
-    var cameraHeight = options.height;
-    var cameraCar = createCameraCar(cameraWidth, cameraHeight);
+    var scale = options.scale;
+    var imgElement   = createImageElement(imgURL, imgContainer.offsetWidth);
+    var cameraWidth = options.width * scale;
+    var cameraHeight = options.height * scale;
+    var cameraWidthWithoutBorder = cameraWidth - 2;
+    var cameraHeightWithoutBorder = cameraHeight - 2;
+    var cameraCarX = options.x || 0;
+    var cameraCarY = options.y || 0;
+    var cameraCar = createCameraCar(cameraWidth, cameraHeight, cameraCarX, cameraCarY);
 
 
     cameraCar.appendChild(imgElement);
-    var camera = imgCamera(imgElement,cameraWidth, cameraHeight );
+    // clip有1px边框，所以camera需要使用减去2后的值
+    var camera = imgCamera(imgElement,cameraWidthWithoutBorder, cameraHeightWithoutBorder );
 
-    camera.translateTo({x:0,y:0,width:cameraWidth, height:cameraHeight});
+    camera.translateTo({x:cameraCarX,y:cameraCarY,width:cameraWidthWithoutBorder, height:cameraHeightWithoutBorder});
     dragAble(cameraCar).startDrag({position:'absolute',range:imgContainer , callBack:{
       moving : cameraMovingCallbac
     }})
@@ -57,11 +67,14 @@ Air.Module("AirUI.ui.Imageclip", function(require){
     imgContainer.appendChild(createMask());
     imgContainer.appendChild(cameraCar);
     function cameraMovingCallbac(e){
-      camera.translateTo({x:cameraCar.offsetLeft,y:cameraCar.offsetTop,width:cameraWidth,height:cameraHeight});
+      camera.translateTo({x:cameraCar.offsetLeft,y:cameraCar.offsetTop,width:cameraWidthWithoutBorder,height:cameraHeightWithoutBorder});
     }
 
     this.getOffset = function(){
-      var result = {}
+      var result = {
+        x: Math.floor(cameraCar.offsetLeft / scale),
+        y: Math.floor(cameraCar.offsetTop / scale)
+      };
       return result;
     }
   }
