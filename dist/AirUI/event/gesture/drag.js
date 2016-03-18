f57b1c315136ec785086bf049f16bf9c504f31af
Zhang youquan (Air) <zhangyouquan@cjia.com>
2016-03-18 16:49:26 +0800

    update

---
+++ dist/AirUI/event/gesture/drag.js
@@ -0,0 +1,10 @@
+Air.Module('AirUI.event.gesture.drag', function(require){
+  functioin eventAction(e){
+      // beacon(document).on('touchStart', function(){
+      //
+      // })
+  };
+  var dragEvent = beacon.createEvent(eventAction);
+
+  return dragEvent;
+});
